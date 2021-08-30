529bcd051d6acc8e248e2c7aa1c453c50b23e6f5
xgICES <990695165@qq.ccom>
2021-08-30 16:24:25 +0800

    auto commit

---
+++ N0te/mianjing.cpp
@@ -0,0 +1,65 @@
+/*0812 tengxun-???database
+ 1
+ * ices's project
+ * linux command check the system resoureces..(top ls df free vmstat ps ..)
+ * cpp11 new features(auto |decltype| constexpr| for(:)| std::move |std::forwarding| rvalue reference| lamda function....)
+ * network osi model & factual standard (7/4/5)
+*/
+
+/*0818 tengxun-csig
+ 1
+ * HTTP :session/cooike
+ * LRU
+ * 1G data : how to sort?
+ * B tree & B+ tree & RB-tree
+ * ways to communication between processes(pipe|name pipes|shared memory|message queue|signal|socket)
+ * code:longest substr that doesn't have repeat char.(DP[i] = ..)
+*/
+
+/*0828 baidu-wu ren che
+ 1
+ * Write a class which is similar to 'map' in STL(use the vector<list<pair<int,int>>>);
+ * Write a class that uses a queue to implement the producer-consumer model(use condition variable,CV);
+ * Write a class based on the 1.1 ,and O(1) lookup required.(binary search tree,BST);
+ 2
+ * code:strcpy(char *src,char *dst);
+ * difference between process and thread.
+ * ices's project : network IO model & packet loss & how to deal with disconnected??
+ * epoll & select
+ * std::vector..
+ * std::map..
+ * RB-tree's shrotcomings
+ * code:qsort(vector<int>,int low,int high) || reverseList(pListNode head)
+ 3
+ * windows hook theory and windows10's hook failed..
+ * career planning
+ * process & thread
+ * sort:memory is 2M,data file is 10M,How?
+ * my turn to ask ..
+*/
+
+/*0829 hao wei lai-IOS
+ 1:basically just C
+ * Steps to compile a program:many details.
+ * local variable in a function will be stored in which area in memory??(in stack,not heap)
+ * declarations and definitions(try talk about extern)
+ * const detail
+ * extern deatil
+ * recursion detail with an example test():(print("--1---"\n);if(num > 3) return;test();print("--2---");)
+ * process & thread
+ * post & get,since we have https,why username and password still need to be encrypted?
+ 2
+ * TCP & UDP
+ * HTTP & HTTPS && header & post & get
+ * process & thread && thread synchronization(mutex,spin_lock,rw_lock,condition variable..)
+ * can process A access an variable int *p which is in process B?? (operating system says you can't)but we still have some ways..
+ * code:find the most recent common superView.(tree problem)
+ 3
+ * intrduce one project,i picked ices's
+ * why not go to vehicle industry??(go to beijing)
+ * why IOS?(very like233..)
+ * how do u think of the national policy about edu industry???(optimistic..)
+ * my turn:Internship opportunity & tutor system & post assignment
+*/
+
+
